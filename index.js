7118d7f4f33508362369bde1080a2770428661f8
Jack <58533031+jackherizsmith@users.noreply.github.com>
2020-11-25 00:14:32 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const input = document.querySelector("input");
 const output = document.querySelector("output");
 async function getAnswers(search) {
   const res = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=AIzaSyBpEjOjqf_x0AhvplpjrDqcQT0oIViKnXc&cx=aea8ce3a3154d4ec4&q="${search}`
+    `https://www.googleapis.com/customsearch/v1?key=&cx=aea8ce3a3154d4ec4&q="${search}`
   );
   const data = await res.json();
   const results = data.items;
